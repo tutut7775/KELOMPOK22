36cfd3e2e5d0a138ffea7a374dc5db9c71daeef7
ika siregar <handayani7772@students.amikom.ac.id>
2016-12-20 23:53:06 +0700

    Update selesai.php

--- selesai.php
+++ selesai.php
@@ -28,7 +28,6 @@
 	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Droid+Serif">
 	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Boogaloo">
 	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Economica:700,400italic">
-
 	
 </head>
 <body>
