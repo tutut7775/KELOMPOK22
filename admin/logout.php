63245ea7e9d2ef1a8d428096cd896c6b0c9226de
ika siregar <handayani7772@students.amikom.ac.id>
2016-12-20 23:59:58 +0700

    Update logout.php

--- admin/logout.php
+++ admin/logout.php
@@ -1,6 +1,6 @@
 <?php
   session_start();
   session_destroy();
-  echo "<script>alert('Anda telah keluar dari halaman administrator'); window.location = '../admin/index.php'</script>";
+  echo "<script>alert('Kamu telah keluar dari halaman administrator'); window.location = '../admin/index.php'</script>";
 
 ?>
