4f995470c526e42c6cc6554b7c42ddf5dd116224
tutut7775 <tutut7775@students.amikom.ac.id>
2016-12-21 23:05:32 +0700

    Update Selesai

--- selesai.php
+++ selesai.php
@@ -189,7 +189,7 @@
 			</div>
 		</div>
 		<hr>
-		<p align="center">CopyRight &copy; 2016 KELOMPOK 22 BRINOCY.CO. ALL Right Reserved</p>
+		<p align="center">CopyRight &copy; 2016 KELOMPOK22 BRINOCY.CO. ALL Right Reserved</p>
 	</footer>
 </body>
 </html>
